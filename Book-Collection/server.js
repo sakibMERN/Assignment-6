c08194e84c2f3509f289bc314261b3cabfba7be8
Sakib <sakib.sr57@gmail.com>
2023-05-11 22:26:34 +0600

    update

---
+++ Book-Collection/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const helmet = require('helmet');
+const app= express();
+
+// const router = require("./router");
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname));
+
+app.get("/",(req,res)=>{
+
+    res.sendFile(__dirname +"./index.html");
+})
+
+
+
+    app.get("/books",(req,res)=>{
+
+let bookArray=[
+
+    {
+        "id":123456,
+        "title":"MERN Book",
+        "author":"Rabbil Hasan",
+        "publishedDate":"2022-06-08"
+    },
+    {
+        "id":123457,
+        "title":"Ostad Book",
+        "author":"Mridul Hasan",
+        "publishedDate":"2023-09-28"
+    },
+    {
+        "id":123458,
+        "title":"Conceptual Book",
+        "author":"Faysal Ahmed",
+        "publishedDate":"2021-12-18"
+    }
+    ]
+
+        res.json(bookArray);
+    })
+
+    app.post("/books",(req,res)=>{
+
+
+
+        res.json(bookArray);
+    })
+
+
+// app.use("/",router);
+
+
+app.listen(8000,()=>{
+    console.log("Server Run Success");
+})
